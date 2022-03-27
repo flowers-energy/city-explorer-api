2182e1547cf2bc67353f2592e723846226333982
flowers-energy <zachary.t.flowers@gmail.com>
2022-03-26 17:03:56 -0700

    working on rendereing movie data

--- server.js
+++ server.js
@@ -3,29 +3,59 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const weatherData = require('./data/weather.json');
+const axios = require('axios');
 
+
+
+app.use(cors());
+
+const PORT = process.env.PORT || 3002;
+const getWeather = async (request, response) => {
+  const lat = request.query.lat;
+  const lon = request.query.lon;
+  //const searchQuery = request.query.searchQuery;
+  const url = `https://api.weatherbit.io/v2.0/forecast/daily/?key=${process.env.WEATHER_API_KEY}&lat=${lat}&lon=${lon}&days=7`;
+  try {
+    const weatherData = await axios.get(url);
+    const forecastArr = weatherData.data.data.map(day => new Forecast(day.valid_date, day.weather.description));
+    response.send(forecastArr);
+  } catch (error) {
+    response.send(error.message);
+  }
+};
+
+app.get('/weather', getWeather);
 
 class Forecast {
   constructor(date, description) {
     this.date = date;
     this.description = description;
   }
+
 }
 
-app.use(cors());
-const PORT = process.env.PORT || 3002;
-app.get('/weather', (request, response) => {
-  const lat = parseInt(request.query.lat);
-  const lon = parseInt(request.query.lon);
-  const searchQuery = request.query.searchQuery;
-  const city = weatherData.find(cityObj => cityObj.city_name.toLowerCase() === searchQuery.toLowerCase());
+const findMovies = async (request, response) => {
+  const movQuery = request.query.query;
+  const url = `https://api.themoviedb.org/3/search/movie/?api_key=${process.env.MOVIE_API_KEY}&language=en-US&query=${movQuery}`;
+  console.log(movQuery);
   try {
-    const forecastArr = city.data.map(day => new Forecast(day.valid_date, day.weather.description));
-    response.send(forecastArr);
+    const movData = await axios.get(url);
+    const movArr = movData.data.results.map(value => new Movies(value));
+    response.send(movArr);
   } catch (error) {
     response.send(error.message);
   }
-});
+};
+
+app.get('/movies', findMovies);
+
+class Movies {
+  constructor(film) {
+    this.title = film.title;
+    this.overview = film.overview;
+    this.img_url = film.poster_path;
+
+  }
+}
 app.listen(PORT, () => console.log(`I'm Alive${PORT}`));
 
